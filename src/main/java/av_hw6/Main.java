59cb2e48abfb30e4c808a984b5a7a8c4d0b8cce6
Alexandr Varenyshev <alex.varenyshev@gmail.com>
2023-11-13 02:56:50 +0300

    First commit hw6

---
+++ src/main/java/av_hw6/Main.java
@@ -0,0 +1,39 @@
+package av_hw6;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Арифметические и сравнительные операторы");
+        byte a = 1;
+        byte b = 2;
+        byte c = 3;
+        byte d = 4;
+        byte e = 5;
+
+        System.out.println("а + b = " + (a + b));
+        System.out.println("d - a = " + (d - a));
+        System.out.println("e % c = " + (e % c));
+        System.out.println("e * e = " + (e * e));
+        System.out.println("d / b = " + (d * b));
+        System.out.println("d / b = " + (d * b));
+        System.out.println("++d = " + (++d));
+        System.out.println("d++ = " + (d++));
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("b >= a = " + (b >= a));
+        System.out.println("b <= a = " + (b <= a));
+
+        System.out.println("Сравнение");
+        int annualturnover = 1000000;
+        byte accession = 5;
+        if (annualturnover <= 1000000 & accession <= 5);
+        System.out.println("Вам присвоен статус Gold");
+
+        if (annualturnover >= 1000000 & accession >= 5);
+        System.out.println("Вам недоступен статус Gold");
+
+
+
+    }
+}
---
+++ src/main/java/lesson/Main.java
@@ -0,0 +1,37 @@
+package lesson;
+
+public class Main {
+    public static void main(String[] args) {
+        String strToBePrinted = "Hello world!";
+
+        //Целочисленные типы данных
+        byte aByte = 100; //-128 ... 127
+        short aShort = 100; // -32768 ... 32767
+        int aInt = 100;
+        Integer aInteger = null;
+        long aLong = 100;
+        // Типы данных с плавающей точкой
+        float aFloat = 1.1f;
+        double aDouble = 1.2; //более точно
+        // Символьный
+        char aChar = '\u0000';
+        // Логический
+        boolean aBooleean = false;
+
+        //Операторыэ
+        // 1. Присвоение =
+        // Арифметические + - * / %
+        // Логические <> <= >= == !=
+
+        //if // если
+        //else // если нет, то
+
+        //Операторы && Несколко условий
+
+        //
+
+
+
+        System.out.println(strToBePrinted);
+    }
+}
